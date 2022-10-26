fba45a71a34d62cc7eda98a5a211080f5b7d1393
emirhandevops <113934798+emirhandevops@users.noreply.github.com>
2022-10-26 10:39:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From Azure Devops" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello EMİRHAN" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
